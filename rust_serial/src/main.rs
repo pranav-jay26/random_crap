f58d0f1f73b6bf33cac543be9cda796a3d4984ec
MiguelX413 <lbpgaming27@gmail.com>
2023-02-02 18:43:26 -0800

    This might work?

--- rust_serial/src/main.rs
+++ rust_serial/src/main.rs
@@ -1,3 +1,17 @@
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("{}", p.port_name);
+    }
+    let mut port = serialport::new("/dev/ttyS0", 9600)
+        .timeout(Duration::from_millis(10))
+        .open()
+        .expect("Failed to open port");
+
+    let mut serial_buf: Vec<u8> = vec![0; 32];
+    let _ = port
+        .read(serial_buf.as_mut_slice())
+        .expect("Found no data!");
 }
