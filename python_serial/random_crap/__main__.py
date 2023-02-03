402a42a41eff2d828f85ae5b649297589f80f15d
MiguelX413 <lbpgaming27@gmail.com>
2023-02-02 18:16:38 -0800

    Add basic code to python_serial

--- python_serial/random_crap/__main__.py
+++ python_serial/random_crap/__main__.py
@@ -1,30 +1,10 @@
 #!/usr/bin/env python3
-import signal
-from time import sleep
-from types import FrameType
-
-import RPi.GPIO as GPIO
-
-PIN = 17
-
-
-def main() -> None:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PIN, GPIO.OUT)
-
-    def signal_handler(sig: int, frame: FrameType):
-        GPIO.cleanup()
-        raise KeyboardInterrupt
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    GPIO.output(PIN, True)
-    while True:
-        sleep(0.5)
-        GPIO.output(PIN, False)
-        sleep(0.5)
-        GPIO.output(PIN, True)
-
+import serial
 
 if __name__ == "__main__":
-    main()
+    ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode("utf-8").rstrip()
+            print(line)
